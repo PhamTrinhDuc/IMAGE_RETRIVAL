8451a69df1655254f425e5afb6d388addc59ef69
PhamTrinhDuc <duc78240@gmail.com>
2024-06-12 10:43:47 +0700

    deploy on gradio app

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from lib import *
-from ViT_processor import ViT_Processor
+from ViT_Processor import ViT_Processor
+from CLIP_Processor import CLIP_Processor
 
-def process_image(img):
-    processor = ViT_Processor(img)
-    grid = processor.run()
+def process(image):
 
-    return grid
+    process = ViT_Processor(image)
+    process.run()
 
+    path_results = "results images query.png"
+    image_results = Image.open(path_results)
 
+    if os.path.exists(path_results):
+        os.remove(path_results)
+        
+    return image_results
+    
 
 # Tạo giao diện Gradio
 iface = gr.Interface(
-    fn=process_image,                # Hàm xử lý ảnh
-    inputs=gr.Image(type="pil"),     # Đầu vào là một ảnh (kiểu PIL)
-    outputs=gr.Image(type="pil"),    # Đầu ra là một ảnh (kiểu PIL)
-    title="Image Processing App",    # Tiêu đề của ứng dụng
-    description="Upload an image and get a processed image in return.",  # Mô tả ứng dụng
+    fn=process,
+    inputs=gr.Image(type="pil"),  # Nhập ảnh dưới dạng đối tượng PIL
+    outputs=[
+        gr.Image(type="pil", label="Results of queried images"),
+    ],
+    title="Image Search Engine",
+    description="Upload an image and get query images"
 )
 # Chạy ứng dụng
 iface.launch(share=True)

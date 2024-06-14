cdab117e20863c53acb33368a0f8c33ed3b5aeac
PhamTrinhDuc <duc78240@gmail.com>
2024-06-14 21:19:12 +0700

    deploy app on gradio

--- app.py
+++ app.py
@@ -1,30 +1,42 @@
 from lib import *
 from ViT_Processor import ViT_Processor
 from CLIP_Processor import CLIP_Processor
+from Module_RAG import llm
 
-def process(image):
+QA_chain = llm.create_Chain_QA()
+# response = QA_chain.invoke({"query": "Liệt kê giúp tôi 5 lò vi sóng"})
+# print(response['result'])
+
+
+def process_image_user(prompt_user, image):
 
     process = ViT_Processor(image)
-    process.run()
+    path_images = process.run()
 
-    path_results = "results images query.png"
-    image_results = Image.open(path_results)
+    images_after_query = [Image.open(path_img) for path_img in path_images]
+    name_product = images_after_query[0].split("/")[1]
+    response = QA_chain.invoke({"query": prompt_user})
 
-    if os.path.exists(path_results):
-        os.remove(path_results)
-        
-    return image_results
-    
+    return response['result'], \
+        images_after_query[0], images_after_query[1], images_after_query[2], images_after_query[3], images_after_query[4]
 
 # Tạo giao diện Gradio
 iface = gr.Interface(
-    fn=process,
-    inputs=gr.Image(type="pil"),  # Nhập ảnh dưới dạng đối tượng PIL
+    fn=process_image_user,
+
+    inputs=[gr.Textbox(lines=2, label="User", placeholder="Nhập yêu cầu của bạn tại đây."),
+            gr.Image(type="pil", label="Nhập ảnh của bạn tại đây")],
+    
     outputs=[
+        gr.Textbox(lines=2, label="Bot", placeholder="Câu trả lời của chúng tôi."),
         gr.Image(type="pil", label="Results of queried images"),
+        gr.Image(type="pil", label="Results of queried images"),
+        gr.Image(type="pil", label="Results of queried images"),
+        gr.Image(type="pil", label="Results of queried images"),
+        gr.Image(type="pil", label="Results of queried images")
     ],
     title="Image Search Engine",
-    description="Upload an image and get query images"
+    description="Upload an image and get query images",
 )
 # Chạy ứng dụng
 iface.launch(share=True)

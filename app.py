f589632a80167ba46d1a6672653c9740f0824b24
PhamTrinhDuc <duc78240@gmail.com>
2024-06-18 10:05:52 +0700

    deploy app on gradio

--- app.py
+++ app.py
@@ -2,42 +2,80 @@ from lib import *
 from ViT_Processor import ViT_Processor
 from CLIP_Processor import CLIP_Processor
 from Module_RAG import llm
+from ultralytics import YOLO
 
-QA_chain = llm.create_Chain_QA()
+# QA_chain = llm.create_Chain_QA()
+weight_path =  "weight_yolo/best_30epoch.pt"
+model = YOLO(weight_path)
 
 
-def process_image_user(prompt_user, image):
+def process_image_user(image):
 
-    process = ViT_Processor(image)
-    path_images = process.run()
+    path_query = "query_image.png"
+    if os.path.exists(path_query): 
+        os.remove(path_query)
+    image.save(path_query)
+
+    ######################## OUPUT FOR ViT RETRIEVAL #######################
+    process = CLIP_Processor(image)
+    path_images, image_names, distance_euclide = process.run()
 
     images_after_query = [Image.open(path_img) for path_img in path_images]
-    name_product = path_images[0].split("/")[1]
+    grid_results_image = Image.open("results images query.png")
 
-    rewrite_prompt = prompt_user + f" .Một số sản phẩm {name_product} của bên tôi."
+    # name_product = path_images[0].split("/")[1]
+    # rewrite_prompt = prompt_user + f" .Một số sản phẩm {name_product} của bên tôi."
+    # response = QA_chain.invoke({"query": rewrite_prompt})
+    
+    ######################## OUPUT FOR YOLO ###############################
+    # xóa ảnh cũ trước khi detec ảnh mới.
+    folder_results = "runs"
+    if os.path.exists(folder_results):
+        shutil.rmtree(folder_results)
 
-    response = QA_chain.invoke({"query": rewrite_prompt})
+    classes = model.names
+    results = model.predict(path_query, verbose=False, save = True)
+    path_image = "runs/detect/predict/" + path_query
+    result_by_yolo = Image.open(path_image)
 
-    return response['result'], \
-        images_after_query[0], images_after_query[1], images_after_query[2], images_after_query[3], images_after_query[4]
+    class_name = None 
+    scores = results[0].boxes.conf.cpu().numpy()
+    # đảm bảo nếu model detect ra nhiều box thì lấy trung bình các score
+    if len(scores) > 1:
+        scores = sum(scores) / len(scores)
+
+    if len(results[0].boxes.cls.numpy()) < 1: # không detect được object
+        class_name = f"Chúng tôi không có sản phẩm nào như vậy"
+    elif scores < 0.8: # score < 0.8
+        class_name = f"Chúng tôi không có sản phẩm nào như vậy || {classes[int(results[0].boxes.cls.numpy()[0])]}"
+    else:
+        class_name = classes[int(results[0].boxes.cls.numpy()[0])]
+
+    
+    return f"Sản phẩm: {class_name} || {scores}", \
+        result_by_yolo, \
+        f"Sản phẩm: {image_names} || {distance_euclide}", \
+        grid_results_image
+        
 
 # Tạo giao diện Gradio
 iface = gr.Interface(
     fn=process_image_user,
 
-    inputs=[gr.Textbox(lines=2, label="User", placeholder="Nhập yêu cầu của bạn tại đây."),
+    inputs=[# gr.Textbox(lines=2, label="User", placeholder="Nhập yêu cầu của bạn tại đây."),
             gr.Image(type="pil", label="Nhập ảnh của bạn tại đây")],
     
     outputs=[
-        gr.Textbox(lines=2, label="Bot", placeholder="Câu trả lời của chúng tôi."),
-        gr.Image(type="pil", label="Results of queried images"),
-        gr.Image(type="pil", label="Results of queried images"),
-        gr.Image(type="pil", label="Results of queried images"),
-        gr.Image(type="pil", label="Results of queried images"),
-        gr.Image(type="pil", label="Results of queried images")
+        gr.Textbox(lines=2, label="By YOLO_v8"),
+        gr.Image(type="pil", label="By YOLO_v8"),
+        gr.Textbox(lines=2, label="By ViT Retrival"),
+        gr.Image(type="pil", label="By ViT Retrival"),
     ],
     title="Image Search Engine",
     description="Upload an image and get query images",
 )
 # Chạy ứng dụng
 iface.launch(share=True)
+
+# image = Image.open("test_query/16241119184849112013705289813434_jpg.rf.5b94d217725c856ae3ecd23f54f6446f.jpg")
+# print(process_image_user(image))
